a7c9fafd52a6f42e145ad422511e316ec1ed0619
Alexey T <info@fialkovod.ru>
2020-06-27 23:01:44 +0300

    new template

---
+++ build/precache-manifest.6e52d531a9ca3cadbdedf4c01be4645e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d678fbc3115c90f7a8a0886391b8f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6fb50f68602ddffcdd6",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b80c0bb98c7d052b74c8",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d6fb50f68602ddffcdd6",
+    "url": "/static/js/2.c30e9ae2.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.c30e9ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80c0bb98c7d052b74c8",
+    "url": "/static/js/main.225936da.chunk.js"
+  },
+  {
+    "revision": "aa211049d16597e99d1b",
+    "url": "/static/js/runtime-main.cb6a7f78.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
